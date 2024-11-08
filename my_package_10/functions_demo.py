05b0baa0d300770c2d199544c85c1ee73b7cd953
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-11-08 12:02:51 +0600

    list

---
+++ my_package_10/functions_demo.py
@@ -0,0 +1,17 @@
+def multiplication(a, b):
+    return a * b
+
+
+print(multiplication(20, 5))
+
+result = multiplication(10, 50)
+print(result)
+
+
+def summation(a=10, b=20):
+    result = a + b
+    print("result:", result)
+
+
+summation()
+summation(100, 500)
---
+++ my_package_10/list-example.py
@@ -0,0 +1,13 @@
+student_id = [1001, 20, 303, 44, 58]
+student_name = ["Student1", "Student2", "Student3"]
+
+dept = [1001, "IT", [1, 2, 3, 4, ["Student1", "Student2", "Student3"]]]
+
+length = len(student_id)
+print(length)
+
+student_id.insert(1, 445)
+print(student_id)
+
+
+
